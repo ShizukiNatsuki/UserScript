386131dd2d34aade4d128d334dc61eb4c604f51d
ShizukiNatsuki <shizuki.natsuki@outlook.com>
2024-12-11 03:57:16 +0800

    Add bangumi-simple-home.user.js

---
+++ Bangumi/bangumi-simple-home.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        Bangumi Simple Home
+// @namespace   moe.shizuki
+// @match       https://bgm.tv/
+// @match       https://chii.in/
+// @match       https://bangumi.tv/
+// @grant       none
+// @version     1.0
+// @author      ShizukiNatsuki
+// ==/UserScript==
+
+document.querySelector("#columnHomeB").remove()
+document.querySelector("#home_calendar").remove()
+document.querySelector("#home_grp_tpc").remove()
